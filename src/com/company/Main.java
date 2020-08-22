32ad283fc6f31be5807a4946ef3861abf1ab0394
marek <marek.sadzynski@gmail.com>
2020-08-22 10:41:32 +0200

    drugie dodanie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,47 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.TimeZone;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String url = "jdbc:mysql://localhost:3306/moviesrental?serverTimezone=" + TimeZone.getDefault().getID();
+        Properties connectionProperties = new Properties();
+        connectionProperties.put("user", "root");
+        connectionProperties.put("password", "password");
+        try (Connection connection = DriverManager.getConnection(url, connectionProperties)) {
+            System.out.println("Nawiązano połączenie");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
-s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
